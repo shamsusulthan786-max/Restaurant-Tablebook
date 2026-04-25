89549b00e9dd07feb8a44883e5d9097c94abb042
shamsusulthan786-max <shamsusulthan786@gmail.com>
2026-04-25 13:53:55 +0400

    Add Cloudinary storage for media files

--- restaurant_booking/settings.py
+++ restaurant_booking/settings.py
@@ -172,3 +172,16 @@ LOGGING = {
         'level': 'WARNING',
     },
 }
+
+# Cloudinary Storage
+import cloudinary
+
+INSTALLED_APPS += ['cloudinary_storage', 'cloudinary']
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
