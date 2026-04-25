a910b48e7296487dd75da951d2ccd7af71c3dd97
shamsusulthan786-max <shamsusulthan786@gmail.com>
2026-04-25 18:13:19 +0400

    Fix placeholder image path

--- restaurant_booking/settings.py
+++ restaurant_booking/settings.py
@@ -182,20 +182,3 @@ LOGGING = {
         'level': 'WARNING',
     },
 }
-
-# Cloudinary
-import cloudinary
-
-cloudinary.config(
-    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    api_key=os.environ.get('CLOUDINARY_API_KEY'),
-    api_secret=os.environ.get('CLOUDINARY_API_SECRET'),
-)
-
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
-}
-
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
